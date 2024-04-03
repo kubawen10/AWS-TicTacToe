77136bbb8b349cba9debb3d96bb0f4f86532f141
Kuba <kubawen10@gmail.com>
2024-04-03 12:16:11 +0000

    finished terraform configuration

--- build/ec2-setup.sh
+++ build/ec2-setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Configure logfile
 LOG_FILE=/logfile.log
 exec > >(tee -a ${LOG_FILE} )
 exec 2>&1
 
-# Install Docker and Docker Compose
+# Install Docker
 sudo apt update -y
 sudo apt install -y docker.io
 sudo service docker start
 sudo usermod -aG docker ubuntu
 
+# Install Docker Compose
 sudo mkdir -p /usr/local/lib/docker/cli-plugins
 sudo curl -sL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
 sudo chown root:root /usr/local/lib/docker/cli-plugins/docker-compose
@@ -26,7 +28,10 @@ PUBLIC_IP=$(curl https://checkip.amazonaws.com)
 # Replace the placeholder in the frontend code with the actual public IP address
 sed -i "s/{{PUBLIC_IP}}/$PUBLIC_IP/g" ./frontend/src/api_connection.js
 
-sudo docker-compose build
+# idk why i cant use docker compose without this two builds
+cd ./frontend
+docker build -t myfrontend:v1 -t myfrontend:latest .
+cd ../backend
+docker build -t mybackend:v1 -t mybackend:latest .
 
-# Run Docker Compose
 sudo docker compose up -d
